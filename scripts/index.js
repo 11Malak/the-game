5001f0ba1c1f85cbee0cb9a39127fdbe72b2f6fa
eduardo <eduvela_94@hotmail.com>
2020-09-28 16:55:45 +0200

    human tank shots 28/09/2020

--- scripts/index.js
+++ scripts/index.js
@@ -20,15 +20,15 @@ for (let index = 0; index < cellCount; index++) {
 
 //initial state alien and human
 let AHUMANTANKPosition = 94;
-let YTANKSHOTPosition = 94;
+let YTANKSHOTPosition = AHUMANTANKPosition;
 //const V = false;
 
 //Ditribution
 const addA = (index) => cells[index].classList.add('AHUMANTANK');
 const removeA = (index) => cells[index].classList.remove('AHUMANTANK');
 
-const addB = (index) => cells[index].classList.add('AHUMANTANK');
-const removeB = (index) => cells[index].classList.remove('AHUMANTANK');
+const addY = (index) => cells[index].classList.add('YTANKSHOT');
+const removeY = (index) => cells[index].classList.remove('YTANKSHOT');
 
 /*switch (key) {
   case 'SpaceBar':
@@ -51,18 +51,13 @@ const handleKeyPress = (event) => {
   const { key } = event;
 
   const x = AHUMANTANKPosition % 10;
-  //const y = Math.floor(BPosition / 10);
-
-  /*const x = YPosition % 10;
-  const y = Math.floor(YPosition / 10);*/
-
+  let YTANKSHOTPosition = AHUMANTANKPosition;
   // 3 - Mover a Bchu de un cuadro al otro dependiendo la orientación y la dirección
   //  3.1  ¿Cómo puedo saber en que coordenas Bchu se encuentra?
 
   removeA(AHUMANTANKPosition);
-
-  //removeY(YPosition);
-
+  console.log(YTANKSHOTPosition);
+  console.log(width);
   switch (key) {
     case 'ArrowRight':
       if (x < width - 1) {
@@ -72,16 +67,27 @@ const handleKeyPress = (event) => {
     case 'ArrowLeft':
       if (x > 0) {
         AHUMANTANKPosition--;
+        console.log('TANQUEMOVIENDOSE');
       }
-
       break;
-    default:
-      console.log('Eso no es una flecha flaco');
+
+    case 'SpaceBar':
+      if (YTANKSHOTPosition > width) {
+        setInterval(() => {
+          removeY(YTANKSHOTPosition);
+          YTANKSHOTPosition -= width;
+          addY(YTANKSHOTPosition);
+          console.log('LASER-MOVIENDOSE');
+          if (YTANKSHOTPosition < width) {
+            clearInterval(YTANKSHOTPosition);
+          }
+        }, 500);
+      }
       break;
   }
+
   addA(AHUMANTANKPosition);
 };
-
 addA(AHUMANTANKPosition);
 
 window.addEventListener('keyup', handleKeyPress);
