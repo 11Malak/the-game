c5433cfa9726782ee1f4f6c64d302b3fee7b42b2
eduardo <eduvela_94@hotmail.com>
2020-09-30 16:11:27 +0200

    Two Alien line explode when it shooted

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,11 @@ const height = width;
 const cellCount = width * height;
 
 const grid = document.querySelector('.grid');
-
 const cells = [];
+let AHUMANTANKPosition = 94;
+let YTANKSHOTPosition = AHUMANTANKPosition;
+let DMIDLANERALIENPosition = [13, 14, 15, 16, 17, 23, 24, 25, 26, 27];
+let EVANGUARDALIENPosition = [33, 34, 35, 36, 37, 43, 44, 45, 46, 47];
 
 // The code do a loop to increase index value until the wanted N to introduce text at each grill
 
@@ -18,69 +21,78 @@ for (let index = 0; index < cellCount; index++) {
   cells.push(cell);
 }
 
+EVANGUARDALIENPosition.forEach((EVANGUARDALIENFleet) => {
+  console.log(EVANGUARDALIENFleet);
+  cells[EVANGUARDALIENFleet].classList.add('EVANGUARDALIEN');
+});
+
+DMIDLANERALIENPosition.forEach((DMIDLANERALIENFleet) => {
+  console.log(DMIDLANERALIENFleet);
+  cells[DMIDLANERALIENFleet].classList.add('DMIDLANERALIEN');
+});
+
+cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
+
 //initial state alien and human
-let AHUMANTANKPosition = 94;
-<<<<<<< HEAD
-let YTANKSHOTPosition = AHUMANTANKPosition;
-=======
-let YTANKSHOTPosition = 94;
->>>>>>> 7d469cd2e2c3bc9d07e1e277c38f97d568457f7e
+
 //const V = false;
 
 //Ditribution
-const addA = (index) => cells[index].classList.add('AHUMANTANK');
-const removeA = (index) => cells[index].classList.remove('AHUMANTANK');
+cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
+cells[AHUMANTANKPosition].classList.remove('AHUMANTANK');
 
-<<<<<<< HEAD
-const addY = (index) => cells[index].classList.add('YTANKSHOT');
-const removeY = (index) => cells[index].classList.remove('YTANKSHOT');
-=======
-const addB = (index) => cells[index].classList.add('AHUMANTANK');
-const removeB = (index) => cells[index].classList.remove('AHUMANTANK');
->>>>>>> 7d469cd2e2c3bc9d07e1e277c38f97d568457f7e
+cells[YTANKSHOTPosition].classList.add('YTANKSHOT');
+cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
 
-/*switch (key) {
-  case 'SpaceBar':
-    V = true;
-    if (y > 0) {
-      YPosition = YPosition - height;
+const EVANGUARDALIENTimming = width;
+const EVANGUARDALIENExplosion = EVANGUARDALIENTimming % 2;
+
+const shoot = (AHUMANTANKPosition) => {
+  let YTANKSHOTPosition = AHUMANTANKPosition;
+  const interval = setInterval(() => {
+    cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
+    YTANKSHOTPosition -= width;
+    cells[YTANKSHOTPosition].classList.add('YTANKSHOT');
+
+    if (cells[YTANKSHOTPosition].classList.contains('EVANGUARDALIEN')) {
+      clearInterval(interval);
+      cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
+
+      cells[YTANKSHOTPosition].classList.remove('EVANGUARDALIEN');
+      cells[YTANKSHOTPosition].classList.add('ALIENEXPLOSION');
+      setTimeout(
+        () => cells[YTANKSHOTPosition].classList.remove('ALIENEXPLOSION'),
+        200,
+      );
     }
-    break;
-    default:
-      console.log('Eso no es una flecha flaco');
-      break;
-  }*/
-
-/*if (V == true) {
-  const addY = (index) => cells[index].classList.add('Y-HUMAN ROCKET');
-  const removeY = (index) => cells[index].classList.remove('Y-HUMAN-ROCKET');
-}*/
+    if (cells[YTANKSHOTPosition].classList.contains('DMIDLANERALIEN')) {
+      clearInterval(interval);
+      cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
+      cells[YTANKSHOTPosition].classList.remove('DMIDLANERALIEN');
+      cells[YTANKSHOTPosition].classList.add('ALIENEXPLOSION');
+      setTimeout(
+        () => cells[YTANKSHOTPosition].classList.remove('ALIENEXPLOSION'),
+        200,
+      );
+    }
+    if (YTANKSHOTPosition < width) {
+      cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
+      clearInterval(interval);
+    }
+  }, 500);
+};
 
 const handleKeyPress = (event) => {
   const { key } = event;
 
   const x = AHUMANTANKPosition % 10;
-<<<<<<< HEAD
-  let YTANKSHOTPosition = AHUMANTANKPosition;
-=======
-  //const y = Math.floor(BPosition / 10);
-
-  /*const x = YPosition % 10;
-  const y = Math.floor(YPosition / 10);*/
-
->>>>>>> 7d469cd2e2c3bc9d07e1e277c38f97d568457f7e
+  // let YTANKSHOTPosition = AHUMANTANKPosition;
   // 3 - Mover a Bchu de un cuadro al otro dependiendo la orientación y la dirección
   //  3.1  ¿Cómo puedo saber en que coordenas Bchu se encuentra?
 
-  removeA(AHUMANTANKPosition);
-<<<<<<< HEAD
+  cells[AHUMANTANKPosition].classList.remove('AHUMANTANK');
   console.log(YTANKSHOTPosition);
   console.log(width);
-=======
-
-  //removeY(YPosition);
-
->>>>>>> 7d469cd2e2c3bc9d07e1e277c38f97d568457f7e
   switch (key) {
     case 'ArrowRight':
       if (x < width - 1) {
@@ -90,36 +102,16 @@ const handleKeyPress = (event) => {
     case 'ArrowLeft':
       if (x > 0) {
         AHUMANTANKPosition--;
-<<<<<<< HEAD
         console.log('TANQUEMOVIENDOSE');
-=======
->>>>>>> 7d469cd2e2c3bc9d07e1e277c38f97d568457f7e
       }
       break;
 
-    case 'SpaceBar':
-      if (YTANKSHOTPosition > width) {
-        setInterval(() => {
-          removeY(YTANKSHOTPosition);
-          YTANKSHOTPosition -= width;
-          addY(YTANKSHOTPosition);
-          console.log('LASER-MOVIENDOSE');
-          if (YTANKSHOTPosition < width) {
-            clearInterval(YTANKSHOTPosition);
-          }
-        }, 500);
-      }
+    case 'w':
+      shoot(AHUMANTANKPosition);
       break;
   }
-<<<<<<< HEAD
-
-  addA(AHUMANTANKPosition);
+  cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
 };
-=======
-  addA(AHUMANTANKPosition);
-};
-
->>>>>>> 7d469cd2e2c3bc9d07e1e277c38f97d568457f7e
-addA(AHUMANTANKPosition);
+cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
 
 window.addEventListener('keyup', handleKeyPress);
