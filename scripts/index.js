926593b300f5f3ba39d8eb4eac35d83ce948f135
eduardo <eduvela_94@hotmail.com>
2020-10-02 12:45:47 +0200

    finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,109 +1,91 @@
 // Do a grid it fill all the screen on cells to guide the scipts by each movements they must do by all the screen.
 
+//// ARRAYS LIST ////
+
+const cells = [];
+let dMidLanerAlien = [13, 14, 15, 16, 17, 23, 24, 25, 26, 27];
+let eVanguardAlien = [33, 34, 35, 36, 37, 43, 44, 45, 46, 47];
+let mdIndex = [53, 54, 55, 56, 57];
+
+//// VARIABLES LIST ////
+
 const width = 10;
 const height = width;
-
 const cellCount = width * height;
-
 const grid = document.querySelector('.grid');
 const score = document.querySelector('span');
-const cells = [];
-let AHUMANTANKPosition = 94;
-let YTANKSHOTPosition = AHUMANTANKPosition;
-let DMIDLANERALIENPosition = [13, 14, 15, 16, 17, 23, 24, 25, 26, 27];
-let EVANGUARDALIENPosition = [33, 34, 35, 36, 37, 43, 44, 45, 46, 47];
-// The code do a loop to increase index value until the wanted N to introduce text at each grill
-Math.floor(Math.random() * EVANGUARDALIENPosition);
+let aHumanTankPosition = 94;
+let yTankShotPosition = aHumanTankPosition;
 let resultado = 0;
+let shooterAreas = mdIndex[Math.floor(Math.random() * mdIndex.length)];
+let xAlienShootPosition = shooterAreas;
+
+//// The code do a FOR loop to increase index value until the wanted N of cell to introduce text by the grill ////
 
 for (let index = 0; index < cellCount; index++) {
   const cell = document.createElement('div');
-  cell.innerText = index;
   grid.appendChild(cell);
   cells.push(cell);
 }
+//// Alien vanguard fleet image spaceship clonning ////
 
-EVANGUARDALIENPosition.forEach((EVANGUARDALIENFleet) => {
-  console.log(EVANGUARDALIENFleet);
-  cells[EVANGUARDALIENFleet].classList.add('EVANGUARDALIEN');
+eVanguardAlien.forEach((eVanguardAlienFleet) => {
+  cells[eVanguardAlienFleet].classList.add('EVANGUARDALIEN');
 });
 
-DMIDLANERALIENPosition.forEach((DMIDLANERALIENFleet) => {
-  console.log(DMIDLANERALIENFleet);
+//// Alien midliner fleet image spaceship clonning ////
+
+dMidLanerAlien.forEach((DMIDLANERALIENFleet) => {
   cells[DMIDLANERALIENFleet].classList.add('DMIDLANERALIEN');
 });
 
-cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
-//initial state alien and human
+///// Human tank add in the human rising /////
 
-//Ditribution
-cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
-cells[AHUMANTANKPosition].classList.remove('AHUMANTANK');
+cells[aHumanTankPosition].classList.add('AHUMANTANK');
 
-cells[YTANKSHOTPosition].classList.add('YTANKSHOT');
-cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
+//// Tank adding elements ////
 
-//scoreText = game.add.text(5, 5, 'Points: 0', {
-//  font: '18px Arial',
-//  fill: '#0095DD',
-//});
+cells[aHumanTankPosition].classList.add('AHUMANTANK');
+cells[aHumanTankPosition].classList.remove('AHUMANTANK');
 
-//function YTANKSHOTHitEVANGUARDALIEN(YTANKSHOT, EVANGUARDALIEN) {
-//  EVANGUARDALIEN.kill();
-//  score += 10;
-//  scoreText.setText('Points: ' + score);
-//  console.log('evanguardalienexplosion');
-//}
-//const V = false;
+cells[yTankShotPosition].classList.add('YTANKSHOT');
+cells[yTankShotPosition].classList.remove('YTANKSHOT');
 
-//for (i = 0; i < EVANGUARDALIEN.children.length; i++) {
-// if (EVANGUARDALIEN.children[i].alive == true) {
-//  count_alive++;
-//  console.log(count_alive);
-//}
-//}
-//if (count_alive == 40) {
-//alert('You won the game, congratulations!');
-//  location.reload();
-//}
+//// Human tank bomb shot animations ////
 
-const EVANGUARDALIENTimming = width;
-const EVANGUARDALIENExplosion = EVANGUARDALIENTimming % 2;
-
-const shoot = (AHUMANTANKPosition) => {
-  let YTANKSHOTPosition = AHUMANTANKPosition;
+const shoot = (aHumanTankPosition) => {
+  let yTankShotPosition = aHumanTankPosition;
   const interval = setInterval(() => {
-    cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
-    YTANKSHOTPosition -= width;
-    cells[YTANKSHOTPosition].classList.add('YTANKSHOT');
+    cells[yTankShotPosition].classList.remove('YTANKSHOT');
+    yTankShotPosition -= width;
+    cells[yTankShotPosition].classList.add('YTANKSHOT');
 
-    if (cells[YTANKSHOTPosition].classList.contains('EVANGUARDALIEN')) {
+    if (cells[yTankShotPosition].classList.contains('EVANGUARDALIEN')) {
       resultado += 10;
       score.textContent = resultado;
       clearInterval(interval);
-      cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
-
-      cells[YTANKSHOTPosition].classList.remove('EVANGUARDALIEN');
-      cells[YTANKSHOTPosition].classList.add('ALIENEXPLOSION');
+      cells[yTankShotPosition].classList.remove('YTANKSHOT');
+      cells[yTankShotPosition].classList.remove('EVANGUARDALIEN');
+      cells[yTankShotPosition].classList.add('ALIENEXPLOSION');
       setTimeout(
-        () => cells[YTANKSHOTPosition].classList.remove('ALIENEXPLOSION'),
+        () => cells[yTankShotPosition].classList.remove('ALIENEXPLOSION'),
         50,
       );
     }
-    if (cells[YTANKSHOTPosition].classList.contains('DMIDLANERALIEN')) {
+    if (cells[yTankShotPosition].classList.contains('DMIDLANERALIEN')) {
       resultado += 20;
       score.textContent = resultado;
       clearInterval(interval);
-      cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
-      cells[YTANKSHOTPosition].classList.remove('DMIDLANERALIEN');
-      cells[YTANKSHOTPosition].classList.add('ALIENEXPLOSION');
+      cells[yTankShotPosition].classList.remove('YTANKSHOT');
+      cells[yTankShotPosition].classList.remove('DMIDLANERALIEN');
+      cells[yTankShotPosition].classList.add('ALIENEXPLOSION');
       setTimeout(
-        () => cells[YTANKSHOTPosition].classList.remove('ALIENEXPLOSION'),
+        () => cells[yTankShotPosition].classList.remove('ALIENEXPLOSION'),
         50,
       );
     }
-    if (YTANKSHOTPosition < width) {
-      cells[YTANKSHOTPosition].classList.remove('YTANKSHOT');
+    if (yTankShotPosition < width) {
+      cells[yTankShotPosition].classList.remove('YTANKSHOT');
       clearInterval(interval);
     }
     if (resultado == 300) {
@@ -113,36 +95,61 @@ const shoot = (AHUMANTANKPosition) => {
   }, 100);
 };
 
+//// Alien shooter spaceship animation laser bombs ////
+
+const alienshoot = (shooterAreas) => {
+  let xAlienShootPosition = shooterAreas;
+  const interval2 = setInterval(() => {
+    cells[xAlienShootPosition].classList.remove('XALIENSHOT');
+    xAlienShootPosition += height;
+    cells[xAlienShootPosition].classList.add('XALIENSHOT');
+
+    if (cells[xAlienShootPosition].classList.contains('AHUMANTANK')) {
+      clearInterval(interval2);
+      cells[xAlienShootPosition].classList.remove('XALIENSHOT');
+      cells[xAlienShootPosition].classList.remove('AHUMANTANK');
+      cells[xAlienShootPosition].classList.add('HUMANEXPLOSION');
+      setTimeout(
+        () => cells[xAlienShootPosition].classList.remove('HUMANEXPLOSION'),
+        100,
+      );
+    }
+    if (xAlienShootPosition > 99) {
+      cells[xAlienShootPosition].classList.remove('XALIENSHOT');
+      clearInterval(interval2);
+    }
+  }, 100);
+};
+
+//// key function reader ////
 const handleKeyPress = (event) => {
   const { key } = event;
 
-  const x = AHUMANTANKPosition % 10;
-  // let YTANKSHOTPosition = AHUMANTANKPosition;
-  // 3 - Mover a Bchu de un cuadro al otro dependiendo la orientación y la dirección
-  //  3.1  ¿Cómo puedo saber en que coordenas Bchu se encuentra?
+  const x = aHumanTankPosition % 10;
 
-  cells[AHUMANTANKPosition].classList.remove('AHUMANTANK');
-  console.log(YTANKSHOTPosition);
-  console.log(width);
+  /// key-board reader to do actions ///
+  cells[aHumanTankPosition].classList.remove('AHUMANTANK');
   switch (key) {
     case 'ArrowRight':
       if (x < width - 1) {
-        AHUMANTANKPosition++;
+        aHumanTankPosition++;
       }
       break;
     case 'ArrowLeft':
       if (x > 0) {
-        AHUMANTANKPosition--;
+        aHumanTankPosition--;
         console.log('TANQUEMOVIENDOSE');
       }
       break;
 
     case 'w':
-      shoot(AHUMANTANKPosition);
-      break;
+      shoot(aHumanTankPosition);
+      shooterAreas = mdIndex[Math.floor(Math.random() * mdIndex.length)];
+      alienshoot(shooterAreas);
+      console.log(mdIndex);
   }
-  cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
+  cells[aHumanTankPosition].classList.add('AHUMANTANK');
 };
-cells[AHUMANTANKPosition].classList.add('AHUMANTANK');
+cells[aHumanTankPosition].classList.add('AHUMANTANK');
 
 window.addEventListener('keyup', handleKeyPress);
